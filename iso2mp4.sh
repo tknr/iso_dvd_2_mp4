b5fa3b3920ace13ceed8d75f4bec1f8ffcff5ee4
tknr <geishagirlskoume@gmail.com>
2019-07-18 22:04:23 +0900

    2019年  7月 18日 木曜日 22:04:23 JST

--- iso2mp4.sh
+++ iso2mp4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
-if [ $(whoami) != "root" ]; then
-        echo "This script must be run as root"
-        exit 1
-fi
+#if [ $(whoami) != "root" ]; then
+#        echo "This script must be run as root"
+#        exit 1
+#fi
 
 export IFS=$'\n'
 
@@ -10,7 +10,7 @@ DATE=`date +%Y_%m_%d`
 argv=("$@")
 CMDNAME=`basename $0`
 
-if [ $# -eq 0 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage : ${CMDNAME} [filename]"
     exit 1
 fi
@@ -22,12 +22,14 @@ MOUNT_DIR="/media/iso"
 
 mkdir -p $MOUNT_DIR || exit 1
 
+umount $MOUNT_DIR
+
 mount -o loop,ro ${abspath} $MOUNT_DIR || exit 1
 
 featureNumber=$(dvdbackup -I -i "$MOUNT_DIR" 2> /dev/null | grep 'Title set containing the main feature is' | sed 's/[^0-9]//g'  )
 featurePadded=$(printf "%02d" $featureNumber)
 files=$(find "$MOUNT_DIR/VIDEO_TS/" | grep $(echo "VTS_"$featurePadded"_[1-9].VOB") | sort | tr '\n' '|' | sed 's/|$//g' )
 
-ffmpeg -i "concat:$files" -y -movflags +faststart -codec:v libx264 -preset:v placebo -acodec aac -b:a 256k "${filename%%.ISO}.mp4"
+ffmpeg -i "concat:$files" -y -deinterlace -movflags +faststart -codec:v libx264 -preset:v placebo -acodec aac -b:a 256k "${filename%%.ISO}.mp4"
 
 umount $MOUNT_DIR
