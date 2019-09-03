30703732699a46302fbbdf3ab64b38dea2965717
tknr <geishagirlskoume@gmail.com>
2019-09-04 01:34:10 +0900

    2019年  9月  4日 水曜日 01:34:10 JST

--- iso2mp4.sh
+++ iso2mp4.sh
@@ -19,6 +19,7 @@ filename=$(basename $1)
 abspath=$(cd $(dirname $1) && pwd)/${filename}
 
 MOUNT_DIR="/media/iso"
+FFMPEG=/usr/local/bin/ffmpeg
 
 mkdir -p $MOUNT_DIR || exit 1
 
@@ -30,6 +31,6 @@ featureNumber=$(dvdbackup -I -i "$MOUNT_DIR" 2> /dev/null | grep 'Title set cont
 featurePadded=$(printf "%02d" $featureNumber)
 files=$(find "$MOUNT_DIR/VIDEO_TS/" | grep $(echo "VTS_"$featurePadded"_[1-9].VOB") | sort | tr '\n' '|' | sed 's/|$//g' )
 
-ffmpeg -i "concat:$files" -y -deinterlace -movflags +faststart -codec:v libx264 -preset:v placebo -acodec aac -b:a 256k "${filename%%.ISO}.mp4"
+$FFMPEG -i "concat:$files" -y -deinterlace -movflags +faststart -codec:v libx264 -preset:v placebo -acodec aac -b:a 256k "${filename%%.ISO}.mp4"
 
 umount $MOUNT_DIR
